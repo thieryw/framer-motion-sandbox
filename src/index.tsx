4fa9cdefa7f16d7c91745ed2ed54348693cdf69d
thieryw <williamalexthiery@hotmail.fr>
2021-07-22 01:22:02 +0200

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,72 @@
-import React from 'react';
+import React, { useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createMakeStyles} from "tss-react";
+import {motion} from "framer-motion";
+
+const {makeStyles} = createMakeStyles({"useTheme": ()=>{}})
+
+document.body.style.margin = "0";
+
+const useStyle = makeStyles()({
+  "root": {
+    "width": "100vw",
+    "height": "100vh",
+    "backgroundColor": "black",
+    "display": "flex",
+    "alignItems": "center",
+    "justifyContent": "center",
+    "flexDirection": "column",
+    "gap": 200
+  },
+  "inner": {
+    "width": 150,
+    "height": 100,
+    "backgroundColor": "white",
+  }
+});
+
+
+function App() {
+  const { classes } = useStyle();
+  const [animationProps, setAnimationProps] = useState({
+    "rotate": 0,
+    "scale": 1
+  });
+
+
+  return (
+    <div className={classes.root}>
+
+      <button
+        onClick={()=> setAnimationProps({
+          "rotate": animationProps.rotate === 0 ? 1 : 0,
+          "scale": animationProps.scale === 1 ? 1.5 : 1
+        })} 
+      >
+        Click Me !
+      </button>
+
+
+      <motion.div
+        className={classes.inner}
+        animate={{
+          ...animationProps,
+          
+        }}
+        transition={{
+          "duration": 2,
+          "delay": 1
+        }}
+
+      >
+
+      </motion.div>
+
+    </div>
+  )
+};
+
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +75,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
